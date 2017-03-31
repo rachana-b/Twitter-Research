e655182fba13029653cdf656171c4863fe772ee4
Thomas Clark <Thomas@nat-oitwireless-inside-vapornet100-c-1543.Princeton.EDU>
2017-03-30 21:00:23 -0400

    Thu Mar 30 21:00:23 EDT 2017

--- util.py
+++ util.py
@@ -1,33 +1,39 @@
-#import requests
+# Scrapes a twitter page containing the list of followees of an account
+# Modify the usr and pwd fields below with your credentials
+# Takes two command line arguments first and last: the number of the first bot and number of last bot
+# assumes that the bots have handles starting with handle_pre (see below) followed by a number
+# between first and last
+# python util.py 0 5 will scrape information for bots springIWthclark0, ..., springIWthclark5
+
 import time
+import sys
 from bs4 import BeautifulSoup
 import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
-# options = webdriver.ChromeOptions()
-# options.add_argument("user-data-dir=/prof")
-# browser = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver", chrome_options=options)
+arg1, arg2 = 0
+if (len(argv) == 3):
+	arg1 = int(sys.argv[1])
+	arg2 = int(sys.argv[2])
+else:
+	print ("Usage: python util.py [start botnum] [end botnum]")
+	return
 
-browser = webdriver.Firefox()
-url = "https://twitter.com/springIWthc3/following"
-time.sleep(1)
-
-# r = request.get(url)
-# soup = BeautifulSoup(r.tet, 'html.parser')
-
-# tweets = [p.text for p in soup.findAll('p', class_='tweet-text')]
-
-# print(tweets)
-
-
-browser.get(url)
-time.sleep(4)
+url_pre = "https://twitter.com/"
+handle_pre = "springIWthc"
+url_post = "/following"
+url = url_pre + handle_pre + "0" + url_post
 
 usr = "thomashikaru"
 pwd = "Clovek79!"
 
+# LOG INTO TWITTER
+browser = webdriver.Firefox()
+time.sleep(1)
+browser.get(url)
+time.sleep(4)
 login1 = browser.find_element_by_class_name("js-username-field")
 time.sleep(1)
 login2 = browser.find_element_by_class_name("js-password-field")
@@ -39,16 +45,26 @@ time.sleep(1)
 browser.find_element_by_css_selector("button.submit.btn.primary-btn").click()
 time.sleep(3)
 
-body = browser.find_element_by_tag_name('body')
-for _ in range(40):
-	body.send_keys(Keys.PAGE_DOWN)
-	time.sleep(0.3)
 
-handles = browser.find_elements_by_class_name('ProfileCard-screenname')
-tweets = browser.find_elements_by_css_selector('p.ProfileCard-bio.u-dir')
+for num in range(arg1, arg2+1):
+	# LOAD THE FOLLOWERS PAGE
+	url = url_pre + handle_pre + str(num) + url_post
+	browser.get(url)
 
-for i in range(len(tweets)):
-	res = "%-20s %s" % (handles[i].text, tweets[i].text)
-	print (res.encode('utf-8', 'ignore'))
+	# SCROLL DOWN TO LOAD ENTIRE FEED
+	body = browser.find_element_by_tag_name('body')
+	for _ in range(100):
+		body.send_keys(Keys.PAGE_DOWN)
+		time.sleep(0.3)
+
+	# FIND THE USER HANDLES AND BRIEF BIOS
+	handles = browser.find_elements_by_class_name('ProfileCard-screenname')
+	tweets = browser.find_elements_by_css_selector('p.ProfileCard-bio.u-dir')
+
+	print ("BOT %d FOLLOWER LIST AND BIOS" % (num,))
+	print ("=======================================================================")
+	for i in range(len(tweets)):
+		res = "%-20s %s" % (handles[i].text, tweets[i].text)
+		print (res.encode('utf-8', 'ignore'))
 
 
