5275db2165fd30ed969b37b94042740620c4bf0c
Rachana Balasubramanian <rachanab@princeton.edu>
2017-06-29 14:32:34 -0400

    Follow recommended for host machine
    - does not work on virtual machine
    - uses chrome driver

--- follow_recommended.py
+++ follow_recommended.py
@@ -1,12 +1,17 @@
+'''
+Author: Rachana Balasubramanian
+Description: For following recommended Twitter users. Run on your host machine.
+'''
+
 import time
 import sys
 import selenium
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import credentials
 from selenium.webdriver.common.keys import Keys
-import random
 
-sample_num = 10
+
+sample_num = 4
 
 num = "0"
 if (len(sys.argv) == 2):
@@ -16,36 +21,50 @@ else:
 	exit()
 
 url = "https://twitter.com/who_to_follow/suggestions"
-usr = "springIWthc"
-pwd = "louisasimpson"
+usr = "summerRB"
+pwd = credentials.PASSWORD
 
 # LOG INTO TWITTER
-browser = webdriver.Firefox()
+# driver = webdriver.PhantomJS(executable_path = "/vagrant/phantomjs-2.1.1-linux-x86_64/bin/phantomjs")
+driver = webdriver.Chrome()
+
 time.sleep(1)
 
-browser.get(url)
-time.sleep(4)
-login1 = browser.find_element_by_class_name("js-username-field")
+driver.get('http://www.twitter.com')
+time.sleep(2)
+login = driver.find_elements_by_xpath('//*[@id="doc"]/div[1]/div/div[1]/div[2]/a[3]')
+login[0].click()
+user = driver.find_elements_by_xpath('//*[@id="login-dialog-dialog"]/div[2]/div[2]/div[2]/form/div[1]/input')
+user[0].send_keys(usr + str(num).zfill(2))
+user = driver.find_element_by_xpath('//*[@id="login-dialog-dialog"]/div[2]/div[2]/div[2]/form/div[2]/input')
+user.send_keys(pwd)
 time.sleep(1)
-login2 = browser.find_element_by_class_name("js-password-field")
-time.sleep(1)
-login1.send_keys(usr + num)
-time.sleep(1)
-login2.send_keys(pwd)
-time.sleep(1)
-browser.find_element_by_css_selector("button.submit.btn.primary-btn").click()
-time.sleep(3)
+LOG = driver.find_elements_by_xpath('//*[@id="login-dialog-dialog"]/div[2]/div[2]/div[2]/form/input[1]')
+LOG[0].click()
+print("Login Sucessfull")
+
+driver.get(url)
 
 # SCROLL DOWN TO LOAD ENTIRE FEED
-body = browser.find_element_by_tag_name('body')
+body = driver.find_element_by_tag_name('body')
 for _ in range(4):
 	body.send_keys(Keys.PAGE_DOWN)
 	time.sleep(0.3)
 
-# get the follow buttons and click the first sample_num of them
-buttons = browser.find_elements_by_class_name('user-actions-follow-button.js-follow-btn.follow-button.btn')
+print("scrolled")
+a= driver.find_element_by_id("stream-items-id")
+lis = a.find_elements_by_css_selector("li") #not all of these are buttons
+
 i = 0
-while (i < sample_num):
-	buttons[i].click()
-	time.sleep(1.0)
-	i += 1
+cnt = 0
+while (cnt < sample_num):
+    print("i = " + str(i) + ", cnt = " + str(cnt))
+    time.sleep(1)
+    try:
+        button = lis[i].find_element_by_css_selector("button")
+        button.click()
+        cnt += 1
+    except Exception as e:
+        print("skip")
+    i += 1
+driver.quit()
